89a44a76c62ce3343667e103b869b974f18d7b71
Jan Brohl <janbrohl@t-online.de>
2016-06-01 13:29:23 +0200

    add mod listing

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ import interfaces.ckan
 import interfaces.netkan
 import datetime
 import os.path
+from simpletal.simpleTALUtils import TemplateRoot, TemplateCache
 from json import loads, dump, load
 from ckan_util import split_version
 
-if True:
+tc = TemplateCache()
+tr = TemplateRoot(os.path.abspath("templates"),
+                  (lambda p: tc.getTemplate(p, "utf-8")))
+
+if False:
     ckan = interfaces.ckan.full()
     netkan = interfaces.netkan.active_full()
     updated = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M")
     for e in ckan:
-        e["x_webtool_id"] = e["identifier"] + "/" + e["version"]
-    ckan.sort(key=(lambda e: (e["identifier"], split_version(e["version"]))))
+        e["x_sortable_version"] = split_version(e["version"])
+    ckan.sort(key=(lambda e: (e["identifier"], e["x_sortable_version"])))
     netkan.sort(key=(lambda e: e["identifier"]))
     with open("static/ckan.min.json", "w", encoding="utf8") as f:
         dump(ckan, f, sort_keys=True, separators=(',', ':'), ensure_ascii=False)
@@ -35,23 +40,53 @@ else:
     updated = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M")
 
 
+def filter_simple(entries, where_eq, latest):
+    if where_eq:
+        fe = [e for e in entries if all(
+            v == e.get(k, None) for k, v in where_eq)]
+    else:
+        fe = entries
+    if not latest:
+        return fe
+    lv = {}
+    for e in fe:
+        k = e["identifier"]
+        if k not in lv or lv[k]["x_sortable_version"] <= e["x_sortable_version"]:
+            lv[k] = e
+    return sorted(lv.values(), key=(lambda e: e["identifier"]))
+
+
 class Root:
 
     @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def ckan(self, known=None):
+    def list(self, known=None, latest="0"):
+        latest = loads(latest)
         if known:
             where_eq = tuple(loads(known).items())
-            return {"data": [e for e in ckan if all(v == e.get(k, None) for k, v in where_eq)], "updated": updated}
-        return {"data": ckan, "updated": updated}
+        else:
+            where_eq = None
+        entries = filter_simple(ckan, where_eq, latest)
+        return tr.expand("list", {"entries": entries})
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def netkan(self, known=None):
+    def ckan(self, known=None, latest="0"):
+        latest = loads(latest)
         if known:
             where_eq = tuple(loads(known).items())
-            return {"data": [e for e in netkan if all(v == e.get(k, None) for k, v in where_eq)], "updated": updated}
-        return {"data": netkan, "updated": updated}
+        else:
+            where_eq = None
+        return {"data": filter_simple(ckan, where_eq, latest), "updated": updated}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def netkan(self, known=None, latest="0"):
+        latest = loads(latest)
+        if known:
+            where_eq = tuple(loads(known).items())
+        else:
+            where_eq = None
+        return {"data": filter_simple(netkan, where_eq, latest), "updated": updated}
 
     @cherrypy.expose
     def index(self):
