46e4ed3dfe652e9a472caa70b7a77794fb53e6d2
HebaruSan <HebaruSan@users.noreply.github.com>
2025-09-01 23:07:58 +0000

    Deploying to gh-pages from @ KSP-CKAN/metadata-webtool@58dbf12bbb91501e43473b530633f01567e5712d 🚀

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
-// auto-generated on 2025-08-31T23:07:30 UTC - see refresh_datajs.py
+// auto-generated on 2025-09-01T23:07:57 UTC - see refresh_datajs.py
 "use strict";
 
-var data_updated = "2025-08-31T23:07:30";
+var data_updated = "2025-09-01T23:07:57";
 
 var mandatory_fields = ["identifier", "name", "abstract", "author", "ksp_version", "license", "version", "download"];
 
