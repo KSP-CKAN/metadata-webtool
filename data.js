efbdc1c4bd02c1a5762de793aee9553181c8fe7d
NetKAN Robot <netkan-bot@ksp-ckan.space>
2026-09-13 00:27:58 +0000

    Deploying to gh-pages from @ KSP-CKAN/metadata-webtool@a6e88151ea196fffe2e24ebd59d5c97f8fa74b98 🚀

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
-// auto-generated on 2026-09-12T00:44:23 UTC - see refresh_datajs.py
+// auto-generated on 2026-09-13T00:27:57 UTC - see refresh_datajs.py
 "use strict";
 
-var data_updated = "2026-09-12T00:44:23";
+var data_updated = "2026-09-13T00:27:57";
 
 var mandatory_fields = ["identifier", "name", "abstract", "author", "ksp_version", "license", "version", "download"];
 
